56503a84249bef150c65e86988dbac63d48705cc
Oliver Liao <rtw0913@gmail.com>
2021-04-27 10:50:34 +0800

    add react

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const app = express();
 
 require('dotenv').config()
 
-app.use(express.static(__dirname + '/my-app/build'));
+app.use(express.static(path.join(__dirname, 'my-app/build')));
 
 app.get('/', function (req, res) {
-    console.log(path.join(__dirname+ '/my-app/build'))
-    res.sendFile(path.join(__dirname));
+    
+  res.sendFile(path.join(__dirname, 'my-app/build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log(path.join(__dirname+ '/my-app/build'))
+    console.log(__dirname, 'my-app/build', 'index.html')
     console.log(process.env.NODE_ENV)
     console.log(process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL)
     console.log(process.env.GOOGLE_PRIVATE_KEY)
